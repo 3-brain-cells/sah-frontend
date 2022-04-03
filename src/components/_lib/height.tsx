ae5d3d2009644efe5c3f5d67001f965c034019ec
Joseph Azevedo <jazevedo620@gmail.com>
2022-04-03 17:16:08 -0400

    try to make it build :)

--- src/components/_lib/height.tsx
+++ src/components/_lib/height.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import { colors } from './colors';
+import { colors } from "./colors";
 
 /**
  * Opaque type used to store height data
@@ -9,26 +9,27 @@ export type Height = number & { __tag__: void };
 
 // Private: don't export
 function makeHeight(rawHeight: number): Height {
-    return rawHeight as Height;
+  return rawHeight as Height;
 }
 
 /**
  * Valid height values
  */
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 export const Height = {
-    h0: makeHeight(0),
-    h5: makeHeight(5),
-    h10: makeHeight(10),
-    h15: makeHeight(15),
-    h20: makeHeight(20),
-    h25: makeHeight(25),
-    h30: makeHeight(30),
-    h35: makeHeight(35),
-    h40: makeHeight(40),
-    h45: makeHeight(45),
-    h50: makeHeight(50),
-    h55: makeHeight(55),
-    h60: makeHeight(60),
+  h0: makeHeight(0),
+  h5: makeHeight(5),
+  h10: makeHeight(10),
+  h15: makeHeight(15),
+  h20: makeHeight(20),
+  h25: makeHeight(25),
+  h30: makeHeight(30),
+  h35: makeHeight(35),
+  h40: makeHeight(40),
+  h45: makeHeight(45),
+  h50: makeHeight(50),
+  h55: makeHeight(55),
+  h60: makeHeight(60),
 };
 
 export const MIN_HEIGHT = Height.h0;
@@ -41,42 +42,42 @@ const HeightContext = React.createContext<Height>(Height.h20);
  * Raises a height value a full amount (+10)
  */
 export function raiseFull(base: Height): Height {
-    return Math.min(base + 10, MAX_HEIGHT) as Height;
+  return Math.min(base + 10, MAX_HEIGHT) as Height;
 }
 
 /**
  * Raises a height value a half amount (+5)
  */
 export function raiseHalf(base: Height): Height {
-    return Math.min(base + 5, MAX_HEIGHT) as Height;
+  return Math.min(base + 5, MAX_HEIGHT) as Height;
 }
 
 /**
  * Lowers a height value a full amount (-10)
  */
 export function lowerFull(base: Height): Height {
-    return Math.max(base + 10, MIN_HEIGHT) as Height;
+  return Math.max(base + 10, MIN_HEIGHT) as Height;
 }
 
 /**
  * Lowers a height value a half amount (-5)
  */
 export function lowerHalf(base: Height): Height {
-    return Math.max(base + 5, MIN_HEIGHT) as Height;
+  return Math.max(base + 5, MIN_HEIGHT) as Height;
 }
 
 /**
  * Gets the higher height between the two provided
  */
 export function higher(a: Height, b: Height): Height {
-    return Math.max(a, b) as Height;
+  return Math.max(a, b) as Height;
 }
 
 /**
  * Gets the lower height between the two provided
  */
 export function lower(a: Height, b: Height): Height {
-    return Math.min(a, b) as Height;
+  return Math.min(a, b) as Height;
 }
 
 /**
@@ -91,49 +92,58 @@ export function lower(a: Height, b: Height): Height {
  * @param floor - Height floor
  * @param contrast - Desired contrast, in height units
  */
-export function contrastFloor(height: Height, floor: Height, contrast: number): Height {
-    const contrastRounded = Math.round(contrast / 5) * 5;
-    return higher((height + contrastRounded) as Height, floor);
+export function contrastFloor(
+  height: Height,
+  floor: Height,
+  contrast: number
+): Height {
+  const contrastRounded = Math.round(contrast / 5) * 5;
+  return higher((height + contrastRounded) as Height, floor);
 }
 
 export type HeightLayerProps = {
-    height: Height;
-    children: React.ReactNode;
+  height: Height;
+  children: React.ReactNode;
 };
 
 /**
  * HeightLayer is used to specify the current height of a subtree
  */
-export default function HeightLayer({ height, children }: HeightLayerProps): React.ReactElement {
-    return <HeightContext.Provider value={height}>{children}</HeightContext.Provider>;
+export default function HeightLayer({
+  height,
+  children,
+}: HeightLayerProps): React.ReactElement {
+  return (
+    <HeightContext.Provider value={height}>{children}</HeightContext.Provider>
+  );
 }
 
 /**
  * Gets the current height
  */
 export function useHeight(): Height {
-    return useContext(HeightContext);
+  return useContext(HeightContext);
 }
 
 /**
  * Converts a height value to the corresponding background color
  */
 export function heightToBackground(height: Height): string {
-    return (
-        {
-            0: colors.background0,
-            5: colors.background5,
-            10: colors.background10,
-            15: colors.background15,
-            20: colors.background20,
-            25: colors.background25,
-            30: colors.background30,
-            35: colors.background35,
-            40: colors.background40,
-            45: colors.background45,
-            50: colors.background50,
-            55: colors.background55,
-            60: colors.background60,
-        }[height as number] ?? '#FF00FF' // Bright magenta
-    );
+  return (
+    {
+      0: colors.background0,
+      5: colors.background5,
+      10: colors.background10,
+      15: colors.background15,
+      20: colors.background20,
+      25: colors.background25,
+      30: colors.background30,
+      35: colors.background35,
+      40: colors.background40,
+      45: colors.background45,
+      50: colors.background50,
+      55: colors.background55,
+      60: colors.background60,
+    }[height as number] ?? "#FF00FF" // Bright magenta
+  );
 }
